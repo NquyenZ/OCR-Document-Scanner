54ffc655c79120aad8148ab4b9623b2f86a32504
nquyenZ <pnguyen12344@gmail.com>
2026-09-26 02:22:59 +0700

    Add image file type validation

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
+supported_extensions = [".jpg", ".jpeg", ".png"]
+
 image_path = input("Enter Image Path: ")
 image_path = Path(image_path)
 
-if image_path.exists():
-    print("Image found")
-else:
+if not image_path.exists():
     print("Can't find image. Please ensure that you entered valid image path")
+elif not image_path.is_file():
+    print("Path is not a file. Please ensure that you entered valid image path")
+elif (image_path.suffix.lower()) not in supported_extensions:
+    print("Unsupported image format. Please ensure that you entered valid image path")
+else:
+    print("Valid image file")
