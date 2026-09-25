570b2dff209d30a61d0400b0fe07685768e70a85
nquyenZ <pnguyen12344@gmail.com>
2026-09-26 02:00:31 +0700

    Add image path validation

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("OCR Document Scanner started successfully")
+from pathlib import Path
+
+image_path = input("Enter Image Path: ")
+image_path = Path(image_path)
+
+if image_path.exists():
+    print("Image found")
+else:
+    print("Can't find image. Please ensure that you entered valid image path")
