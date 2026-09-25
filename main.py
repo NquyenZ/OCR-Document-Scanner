d409f5f11ee5a4bb9f8cdf54b7823929b18d894e
nquyenZ <pnguyen12344@gmail.com>
2026-09-26 02:35:53 +0700

    Add image content validation with Pillow and Requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from PIL import Image, UnidentifiedImageError
 
 supported_extensions = [".jpg", ".jpeg", ".png"]
 
@@ -9,7 +10,13 @@ if not image_path.exists():
     print("Can't find image. Please ensure that you entered valid image path")
 elif not image_path.is_file():
     print("Path is not a file. Please ensure that you entered valid image path")
-elif (image_path.suffix.lower()) not in supported_extensions:
+elif image_path.suffix.lower() not in supported_extensions:
     print("Unsupported image format. Please ensure that you entered valid image path")
 else:
-    print("Valid image file")
+    try:
+        with Image.open(image_path) as image:
+            image.verify()
+
+            print("Valid image file")
+    except UnidentifiedImageError:
+        print("Invalid or corrupted image file. Please try again")
